321352f05eb25b0e9554f7a3c9ca91c9d25e752a
Yair Farina <farinayair1@gmail.com>
2023-08-02 19:55:30 -0300

    Comentando-y-corrigiendo-codigo

--- DBZ-GAME.py
+++ DBZ-GAME.py
@@ -5,8 +5,6 @@ from Character import Character
 print("****BIENVENIDO A DRAGON BALL Z GAME FUSION*****")
 
 pj_list = []
-name_list = []
-i = 0
 menu = "0"
 while menu != "4":
     print("""Eliga una de las siguientes opciones:\n
@@ -22,13 +20,11 @@ while menu != "4":
             fuerza = int(input("Ingrese la fuerza de su PJ: "))
             velocidad = int(input("Ingrese la velocidad de su PJ: "))
             vida = int(input("Ingrese la vida de su PJ: "))
-            pj_list.append(Character(nombre, fuerza, velocidad, vida))
-            for pj in pj_list:
-                name_list.append(pj.nombre)
+            pj_list.append(Character(nombre, fuerza, velocidad, vida)) # Acá estamos agregamos el objeto de la clase Character a una lista
             print("Su personaje ha sido creado exitosamente .....\n")
             print("Enter para continuar")
             input()
-            os.system('cls')
+            os.system('cls') # Limpiamos la cosnsola
         except:
             print("No se ha podido crear su personaje")
             print("Enter para continuar")
@@ -38,14 +34,14 @@ while menu != "4":
     elif menu == "2":
         try: 
             print("Debe elegir a que personajes quiere fusionar: ")   
-            for count, value in enumerate(pj_list):
+            for count, value in enumerate(pj_list):  # Aca estamos enumerando los elementos de la lista atravez de la función enumerate para que el usuario pueda elegir uno
                 print(count, value)
-            one = int(input("Primer personaje a fusionar(Eliga uno de la lista ingresando su n°): "))
-            two = int(input("Segundo personaje a fusionar: "))
-            fusion = pj_list[one] + pj_list[two]
-            pj_list.append(fusion)
+            one = int(input("Primer personaje a fusionar(Eliga uno de la lista ingresando su n°): ")) # Almacenamos el primer pj que eligió el usuario
+            two = int(input("Segundo personaje a fusionar: ")) # Almacenamos el primer pj que eligió el usuario
+            fusion = pj_list[one] + pj_list[two]  #En esta variable guardamos la fusión de los personajes que eligio el usuario anteriormente
+            pj_list.append(fusion) # Agregamos el personaje a la lista de personajes
             print(f"""Felicidades, su fusión ha sido exitosa\n
-            He aquí el personaje creado {fusion}""")
+            He aquí el personaje creado {fusion}""") # Mostramos el personaje ya fusionado junto con sus propiedades 
             print("Enter para continuar")
             input()
             os.system('cls')
@@ -57,11 +53,11 @@ while menu != "4":
 
     elif menu == "3":
         print("Aquí una lista de los personajes existentes")
-        if len(pj_list) == 0:
+        if len(pj_list) == 0: # A travez del método len() podemos contar los elementos de la lista(En este caso si no hay ningun elemento imprimiremos los siguiente)
             print("Usted aún no ha creado ningún personaje")
         else:
             print(f"""He aquí la lista de los personajes disponibles :\n
-                    {pj_list}""")
+                    {pj_list}""")  # Acá estamos mostrando la lista con los personajes ya creados anteriormente
         print("Enter para continuar")
         input()
         os.system('cls')
@@ -69,6 +65,7 @@ while menu != "4":
     elif menu == "4":
         print("El juego ha finalizado")
         break
+    
 
     else:
         print("Porfavor ,Debe ingresar un numero correcto")
